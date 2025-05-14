9f039a735438d25e382cf8f762dc04503aee6b6f
CarlaLDC <carla9lima0carvalho@gmail.com>
2025-05-14 01:09:15 -0300

    editando desafio um

--- src/App.java
+++ src/App.java
@@ -4,14 +4,9 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner ler = new Scanner(System.in);
 
-        String jogo = Jogo.JogoJereca("");
-
-        int escolha = 0;
+        String[] jogo = Jogo.JogoJereca("");
 
         System.out.println(jogo);
-
-        // jogo(escolha);
-           
         
     }
 
--- src/Jogo.java
+++ src/Jogo.java
@@ -2,33 +2,34 @@ import java.util.Scanner;
 
 public class Jogo {
     
- public static String JogoJereca(String escolha) {
+ public static String[] JogoJereca(String escolha) {
         Scanner ler = new Scanner(System.in);
 
-        do {
-            String menu = Menu.MostrarMenu("");
-            String arrays = Menu.Arrays("");
-            
-            String[] relacionalOculto = {"operadores", "relacionais", "comparam", "valores", "retornam", "==",  "!=",  ">", "<", ">=","<="};
-            /*
-            * Os operadores relacionais comparam dois valores e retornam true ou false, como == (igual), != (diferente), > (maior), < (menor), >=      
-            * (maior ou igual) e <= (menor ou igual).
-            */
+        String menu = Menu.MostrarMenu("");
+        // String arrays = Menu.Arrays("");
+        
+        String[] relacionalOculto = {"operadores", "relacionais", "comparam", "valores", "retornam", "==",  "!=",  ">", "<", ">=","<="};
+        /*
+        * Os operadores relacionais comparam dois valores e retornam true ou false, como == (igual), != (diferente), > (maior), < (menor), >=      
+        * (maior ou igual) e <= (menor ou igual).
+        */
 
-            String[] operadorRelacional = {"Os", "**********", "***********", "********", "dois", "*******", "e", "********", "true", "ou", "false", "como",  "**",  "**",  "*", "*", "**", "e", ""};
-            /*
-            * Os operadores relacionais comparam dois valores e retornam true ou false, como == (igual), != (diferente), > (maior), < (menor), >=      
-            * (maior ou igual) e <= (menor ou igual).
-            */
+        String[] operadorRelacional = {"Os", "**********", "***********", "********", "dois", "*******", "e", "********", "true", "ou", "false", "como",  "**",  "**",  "*", "*", "**", "e"};
+        /*
+        * Os operadores relacionais comparam dois valores e retornam true ou false, como == (igual), != (diferente), > (maior), < (menor), >=      
+        * (maior ou igual) e <= (menor ou igual).
+        */
+
+        do {
 
             System.out.println(menu);
     
             escolha = ler.nextLine();
 
             switch (escolha) {
-                case 1:
+                case "1":
                     
-                    while (escolha == 1) {
+                    while (escolha == "1") {
                         System.out.println("Este é o desafio 1 ");
                         
                         for (String item : operadorRelacional) {
@@ -43,16 +44,10 @@ public class Jogo {
             }
             
 
-        } while (escolha => 3);
+        } while (escolha == "3");
 
 
-        return escolha;
-
-
-
-
-
-
+        return operadorRelacional;
 
     }
 
