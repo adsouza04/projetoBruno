b8ed10315d0b12b0d53ac19d623060dc80af1b79
tiphy <souzatiphany55@gamil.com>
2025-05-15 14:22:22 -0300

    desafio um

--- src/Jogo.java
+++ src/Jogo.java
@@ -8,38 +8,73 @@ public class Jogo {
         String menu = Menu.MostrarMenu("");
         // String arrays = Menu.Arrays("");
         
-        String[] relacionalOculto = {"operadores", "relacionais", "comparam", "valores", "retornam", "==",  "!=",  ">", "<", ">=","<="};
+        String[] relacionalOculto = {"Os","operadores",  "relacionais", "comparam", "dois", "valores", "e", "retornam", "true", "ou", "false,", "como", "==",  "!=",  ">", "<", ">=", "e", "<="};
         /*
         * Os operadores relacionais comparam dois valores e retornam true ou false, como == (igual), != (diferente), > (maior), < (menor), >=      
         * (maior ou igual) e <= (menor ou igual).
         */
 
-        String[] operadorRelacional = {"Os", "**********", "***********", "********", "dois", "*******", "e", "********", "true", "ou", "false", "como",  "**",  "**",  "*", "*", "**", "e"};
+        String[] operadorRelacional = {"Os", "**********", "***********", "********", "dois", "*******", "e", "********", "true", "ou", "false,", "como",  "**",  "**",  "*", "*", "**", "e", "**"};
         /*
         * Os operadores relacionais comparam dois valores e retornam true ou false, como == (igual), != (diferente), > (maior), < (menor), >=      
         * (maior ou igual) e <= (menor ou igual).
         */
 
         do {
-
-            System.out.println(menu);
-    
+            System.out.print("Escolha uma opção: ");
             escolha = ler.nextLine();
-
+            System.out.println();
             switch (escolha) {
                 case "1":
-                    
-                    while (escolha == "1") {
-                        System.out.println("Este é o desafio 1 ");
+                    System.out.println("Este é o desafio 1 ");
+                    boolean completo = false;
+                    while (!completo) {
                         
+                        System.out.println("\nFrase atual:");
                         for (String item : operadorRelacional) {
-                            System.out.println(item);
+                            System.out.print(item + " ");
+                        }
+                        System.out.println();
+
+                        System.out.print("Digite uma palavra para tentar preencher: ");
+                        String palpite = ler.nextLine();
+
+                        boolean acertou = false;
+                        for (int i = 0; i < relacionalOculto.length && i < operadorRelacional.length; i++) {
+                            if ((operadorRelacional[i].contains("*")) && relacionalOculto[i].equalsIgnoreCase(palpite)) {
+                                operadorRelacional[i] = relacionalOculto[i];
+                                acertou = true;
+                            }
+                        }
+
+                        if (acertou) {
+                            System.out.println("\nPalavra inserida.");
+                        } else {
+                            System.out.println("\nNenhuma palavra corresponde ao seu palpite.");
+                        }
+
+                        
+                        completo = true;
+                        for (int i = 0; i < relacionalOculto.length && i < operadorRelacional.length; i++) {
+                            if (operadorRelacional[i].contains("*")) {
+                                completo = false;
+                                break;
+                            }
                         }
                     }
 
+                  
+                    System.out.println("\nParabéns! Você completou:");
+                    for (String item : operadorRelacional) {
+                        System.out.print(item + " ");
+                    }
+                    System.out.println();
                     break;
-            
+                case "2":
+                    System.out.println("Saindo do jogo...");
+                    break;
                 default:
+                    System.out.println("Opção inválida. Tente novamente.");
                     break;
             }
             
